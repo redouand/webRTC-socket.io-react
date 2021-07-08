e4dc8b5bec082858b0b65abb469ef09a52395a01
redouand <bnitamoublida47@gmail.com>
2021-07-08 23:35:16 +0100

    init commit

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import CreateRoom from "./routes/CreateRoom";
+import Room from "./routes/Room";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={CreateRoom} />
+          <Route path="/room/:roomID" component={Room} />
+        </Switch>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
